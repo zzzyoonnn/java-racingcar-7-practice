daaf99077268d8c5e868f20043f6795d4fd7e7c9
zzzyoonnn <dwsrh03@gmail.com>
2026-03-10 18:44:07 +0900

    feat: 자동차 이름 입력 유효성 검사 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,7 +7,15 @@ import java.util.List;
 public class InputView {
   public List<String> getCarNames() {
     System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-    return Arrays.asList(Console.readLine().split(","));
+    String input = Console.readLine();
+    validateCarNames(input);
+    return Arrays.asList(input.split(","));
+  }
+
+  private void validateCarNames(String input) {
+    if (input == null || input.isBlank()) {
+      throw new IllegalArgumentException("자동차 이름을 입력해주세요.");
+    }
   }
 
   public int getCount() {
