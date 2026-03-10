dd68e6dc85890e7f86dc8573ee43beb09223498f
zzzyoonnn <dwsrh03@gmail.com>
2026-03-10 18:06:45 +0900

    refactor: Race 도메인 분리

--- src/main/java/racingcar/domain/Race.java
+++ src/main/java/racingcar/domain/Race.java
@@ -1,4 +1,38 @@
 package racingcar.domain;
 
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
 public class Race {
+  private final List<Car> cars;
+  private final Random random;
+
+  public Race(List<Car> cars) {
+    this.cars = cars;
+    this.random = new Random();
+  }
+
+  public void moveCars() {
+    for (Car car : cars) {
+      if (random.nextInt(10) >= 4) {
+        car.move();
+      }
+    }
+  }
+
+  public List<Car> findWinners() {
+    int maxDistance = cars.stream()
+            .mapToInt(Car::getPosition)
+            .max()
+            .orElse(0);
+
+    return cars.stream()
+            .filter(car -> car.getPosition() == maxDistance)
+            .collect(Collectors.toList());
+  }
+
+  public List<Car> getCars() {
+    return cars;
+  }
 }
